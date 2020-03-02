79fd2ad455d5fcc2d462febd3e2125b8fff56a29
Marco Podda <marco.podda@di.unipi.it>
2020-03-02 23:41:04 +0100

    updated

--- utils/plots.py
+++ utils/plots.py
@@ -10,11 +10,11 @@ sns.set_style('whitegrid', {'axes.grid': False})
 params = {
     'font.family': 'sans-serif',
     'font.sans-serif': ['Helvetica'],
-    'legend.fontsize': 'x-small', 
-    'legend.handlelength': 1, 
+    'legend.fontsize': 'x-small',
+    'legend.handlelength': 1,
     'legend.handletextpad': 0.2,
-    'legend.columnspacing': 0.8, 
-    'xtick.labelsize': 'x-small', 
+    'legend.columnspacing': 0.8,
+    'xtick.labelsize': 'x-small',
     'ytick.labelsize': 'x-small'}
 plt.rcParams.update(params)
 
@@ -34,19 +34,19 @@ def plot_property(df, name, prop, ax=None):
     new_names = dict([(p, p.upper()) for p in props])
     df.rename(columns=new_names, inplace=True)
     sns.distplot(df[prop.upper()][df.who==name], hist=False, label=name, ax=ax)
-    ax = sns.distplot(df[prop.upper()][df.who=='F2M'], hist=False, label=MODEL, ax=ax)
+    ax = sns.distplot(df[prop.upper()][df.who==MODEL], hist=False, label=MODEL, ax=ax)
     ax.set_aspect(1.0/ax.get_data_ratio()*ratio)
 
 
 def plot_count(df, name, feat, ax=None):
     s1 = df[feats[feat]][df.who==name].mean(axis=0)
-    s2 = df[feats[feat]][df.who=='F2M'].mean(axis=0)
+    s2 = df[feats[feat]][df.who==MODEL].mean(axis=0)
     data = pd.DataFrame([s1, s2], index=[name, MODEL])
     ax = data.plot(kind='bar', stacked=True, ax=ax, rot=0)
     ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.9),
           ncol=len(feats[feat]), framealpha=0, borderpad=1, title=feat.upper())
-    
-    
+
+
 def plot_counts(df, dataset_name):
     fig, axs = plt.subplots(1, 3)
     for i, f in enumerate(feats):
@@ -68,4 +68,4 @@ def plot_paper_figures(run_dir):
     df = pd.read_csv(os.path.join(run_dir, 'results/samples/aggregated.csv'))
     plot_counts(df, dataset_name)
     plot_props(df, dataset_name)
-        
+
